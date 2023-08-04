23718e0b52cd457edd5ea08fb63347e12eab5b95
Dinesh <dineshr93@gmail.com>
2023-08-04 16:39:39 +0200

    with mux get post and param pattern restriction

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dineshr93/sw360API
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/a", test)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/a", test).Methods(http.MethodGet)
+
+	// restrict the url param to 0 to 9
+	r.HandleFunc("/a/{b:[0-9]+}", test2).Methods(http.MethodGet)
+
+	r.HandleFunc("/a/{b:[0-9]+}", test3).Methods(http.MethodPost)
+
+	// http.HandleFunc("/a", test)
+	log.Fatal(http.ListenAndServe(":80", r))
 
 }
 func test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
+}
+func test2(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, vars["b"])
+}
+func test3(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Post "+vars["b"])
 }

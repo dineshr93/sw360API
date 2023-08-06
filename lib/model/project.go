f112da7777ce9511ec019301daaa66476be2dc0d
Dinesh <dineshr93@gmail.com>
2023-08-06 19:11:03 +0200

    omitempty in project model

--- lib/model/project.go
+++ lib/model/project.go
@@ -24,11 +24,11 @@ type ProjectDetail struct {
 	} `json:"externalIds,omitempty"`
 	AdditionalData struct {
 	} `json:"additionalData,omitempty"`
-	CreatedOn          string `json:"createdOn"`
-	BusinessUnit       string `json:"businessUnit"`
-	State              string `json:"state"`
+	CreatedOn          string `json:"createdOn,omitempty"`
+	BusinessUnit       string `json:"businessUnit,omitempty"`
+	State              string `json:"state,omitempty"`
 	Tag                string `json:"tag,omitempty"`
-	ClearingState      string `json:"clearingState"`
+	ClearingState      string `json:"clearingState,omitempty"`
 	ProjectResponsible string `json:"projectResponsible,omitempty"`
 	Roles              struct {
 	} `json:"roles,omitempty"`
@@ -42,7 +42,7 @@ type ProjectDetail struct {
 	SystemTestEnd                 string           `json:"systemTestEnd,omitempty"`
 	DeliveryStart                 string           `json:"deliveryStart,omitempty"`
 	PhaseOutSince                 string           `json:"phaseOutSince,omitempty"`
-	EnableSvm                     bool             `json:"enableSvm"`
+	EnableSvm                     bool             `json:"enableSvm,omitempty"`
 	EnableVulnerabilitiesDisplay  bool             `json:"enableVulnerabilitiesDisplay"`
 	ClearingSummary               string           `json:"clearingSummary,omitempty"`
 	SpecialRisksOSS               string           `json:"specialRisksOSS,omitempty"`
@@ -50,31 +50,31 @@ type ProjectDetail struct {
 	SpecialRisks3RdParty          string           `json:"specialRisks3rdParty,omitempty"`
 	DeliveryChannels              string           `json:"deliveryChannels,omitempty"`
 	RemarksAdditionalRequirements string           `json:"remarksAdditionalRequirements,omitempty"`
-	ProjectType                   string           `json:"projectType"`
-	Visibility                    string           `json:"visibility"`
+	ProjectType                   string           `json:"projectType,omitempty"`
+	Visibility                    string           `json:"visibility,omitempty"`
 	LinkedProjects                []LinkedProjects `json:"linkedProjects,omitempty"`
 	LinkedReleases                []LinkedReleases `json:"linkedReleases,omitempty"`
 	Links                         struct {
 		Self struct {
-			Href string `json:"href"`
-		} `json:"self"`
-	} `json:"_links"`
+			Href string `json:"href,omitempty"`
+		} `json:"self,omitempty"`
+	} `json:"_links,omitempty"`
 	Embedded struct {
 		ClearingTeam string `json:"clearingTeam"`
 		CreatedBy    string `json:"createdBy"`
-	} `json:"_embedded"`
+	} `json:"_embedded,omitempty"`
 	VendorID string `json:"vendorId,omitempty"`
 }
 
 type Project struct {
 	Embedded struct {
-		Sw360Projects []ProjectDetail `json:"sw360:projects"`
-	} `json:"_embedded"`
+		Sw360Projects []ProjectDetail `json:"sw360:projects,omitempty"`
+	} `json:"_embedded,omitempty"`
 	Links struct {
 		Curies []struct {
-			Href      string `json:"href"`
-			Name      string `json:"name"`
-			Templated bool   `json:"templated"`
-		} `json:"curies"`
-	} `json:"_links"`
+			Href      string `json:"href,omitempty"`
+			Name      string `json:"name,omitempty"`
+			Templated bool   `json:"templated,omitempty"`
+		} `json:"curies,omitempty"`
+	} `json:"_links,omitempty"`
 }

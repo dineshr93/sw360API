6b1d715c89deb6fb54bc2edbc5c5a927b1d5dc08
Dinesh <dineshr93@gmail.com>
2023-08-07 00:30:28 +0200

    add delete project with test cod in main

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/dineshr93/sw360api
 
 go 1.20
 
-require github.com/dineshr93/sw360api/lib v0.0.0-20230806213352-5beb5d9c6d31
-
-require github.com/dineshr93/sw360api/lib/model v0.0.0-20230806213352-5beb5d9c6d31 // indirect
+require (
+	github.com/dineshr93/sw360api/lib v0.0.0-20230806222251-af72f94899c2
+	github.com/dineshr93/sw360api/lib/model v0.0.0-20230806222251-af72f94899c2
+)
 
 // indirect
 require gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"strconv"
@@ -21,12 +22,16 @@ var (
 	Token   = ""
 )
 
+func prettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
 func main() {
 	apicfg := lib.Sw360(configfile)
 
 	fmt.Println("No of projects in server: ", apicfg.GetTotalProjects())
 
-	fmt.Println("========================================")
+	fmt.Println("=================FindProjectswithName=======================")
 	no, names := apicfg.FindProjectswithName("ada")
 
 	fmt.Println("No of projects matched is: ", no)
@@ -35,7 +40,7 @@ func main() {
 		fmt.Println(name)
 	}
 
-	fmt.Println("========================================")
+	fmt.Println("=================GetProjectDetails=======================")
 	// err, data := apicfg.GetProjectDetails("ZF_Middleware_Program", "IOLITHFBL_0500")
 	err, ProjectDetail := apicfg.GetProjectDetails("gradle_single", "1.0")
 	if err != nil {
@@ -44,7 +49,7 @@ func main() {
 
 		fmt.Println(ProjectDetail.LinkedProjects)
 	}
-	fmt.Println("========================================")
+	fmt.Println("===================GetLinkedReleases=====================")
 	err, releases := apicfg.GetLinkedReleases("gradle_single", "1.0")
 	if err != nil {
 		log.Fatalln(err)
@@ -56,7 +61,7 @@ func main() {
 			fmt.Println(strconv.Itoa(i), release.Name, " ", release.Version)
 		}
 	}
-	fmt.Println("========================================")
+	fmt.Println("=================GetLinkedProjects=======================")
 	err, projects := apicfg.GetLinkedProjects("gradle_single", "1.0")
 	if err != nil {
 		log.Fatalln(err)
@@ -66,14 +71,14 @@ func main() {
 			fmt.Println(strconv.Itoa(i), project.Name, " ", project.Version)
 		}
 	}
-	fmt.Println("========================================")
+	fmt.Println("================GetProjectlink========================")
 	err, projectlink := apicfg.GetProjectlink("gradle_single", "1.0")
 	if err != nil {
 		log.Fatalln(err)
 	} else {
 		fmt.Println(projectlink)
 	}
-	fmt.Println("========================================")
+	fmt.Println("=================GetLinkedReleasesTransitive=======================")
 	err, releases = apicfg.GetLinkedReleasesTransitive("gradle_single", "1.0")
 	if err != nil {
 		log.Fatalln(err)
@@ -85,6 +90,22 @@ func main() {
 			fmt.Println(strconv.Itoa(i), release.Name, " ", release.Version)
 		}
 	}
+	fmt.Println("==================CreateProject======================")
+
+	// data := model.ProjectCreationModel{Name: "fromDinesh", Version: "2.0", Description: "Sample Project"}
+
+	// err, msg := apicfg.CreateProject(data)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(prettyPrint(msg))
+	fmt.Println("=================DeleteProject=======================")
+	// err, msg := apicfg.DeleteProject("fromDinesh", "2.0")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(msg)
 	fmt.Println("========================================")
+
 	fmt.Println("========================================")
 }
